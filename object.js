67c56e6a58f7e15c737e803edd4bb0b59e60aed8
RM <rajlaxmi79812@gmail.com>
2025-01-02 16:12:57 +0530

    obje 1 class

--- object.js
+++ object.js
@@ -32,8 +32,58 @@
 function blink(...btn){
     return btn
 }
-console.log(blink("milk", "sugar"))
+// console.log(blink("milk", "sugar"))
 
 //  console.log(arr.sort());
 //  console.log(Math.min(...arr));
  
+
+
+var obj = {
+    name: "sachin",
+}
+var obj2 = {...obj}
+console.log(obj2.name);
+
+
+// const person = {
+//     name: "Alice",
+//     age: 25,
+//     city: "New York",
+// };
+
+// console.log(person.name, person.age , person.city)
+// const { name, age, city } = person;
+
+// console.log(name, age, city); 
+// Output: Alice 25 New York
+
+
+const person = {
+    name: "John",
+    age: 30,
+};
+
+
+const key = "name";
+person[key] = "Doe"; // Dynamically access and update the "name" property
+console.log(person);
+
+
+
+
+
+
+   Object.keys(person).forEach((key) => {
+       console.log(`${key}: ${person[key]}`);
+   });
+
+
+   for (const [key, value] of Object.entries(person)) {
+       console.log(`${key}: ${value}`);
+   }
+
+   Object.values(person).forEach((value) => {
+       console.log(value+1);
+   });
+   
